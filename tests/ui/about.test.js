e04c2ecf6d857a3e3ec2cd690d9749962d07fe7f
christinanikolova <christinanikolova@github.com>
2024-10-13 11:36:09 +0300

    fix ui tests

--- tests/ui/about.test.js
+++ tests/ui/about.test.js
@@ -4,5 +4,5 @@ test("Check about page", async ({ page }) => {
   await page.goto("https://exam-prep-movies-10et.onrender.com/about");
   const heading = await page.$("h1");
   const text = await heading.textContent();
-  expect(text).toBe("About");
+  expect(text).toBe("About Page");
 });
